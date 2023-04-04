d298c35e9382f24233aa9057160a7ae165e4683b
baikguk <baikgukhyun@gmail.com>
2023-04-04 16:19:43 +0900

    JDBCTemplate 및 스프링 통합테스트

--- hello-spring/src/main/java/hello/hellospring/SpringConfigure.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfigure.java
@@ -1,15 +1,29 @@
 package hello.hellospring;
 
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration// 컴포넌트 말고 자바로 빈등록하는 방법 @Service @Autowired가 동시에 되는 느낌
             //컨트롤러는 컴포넌트 스캔으로 해야함
 public class SpringConfigure   {
+
+    private DataSource dataSource; // 데이터 베이스와 연결할 수 있는 정보
+
+    @Autowired
+    public SpringConfigure(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -17,6 +31,7 @@ public class SpringConfigure   {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource); // 자바의 다형성을 통해 쉽게 변경 가능 (assembly 조립)
     }
 }
---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,58 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional // 테스트 실행시 트랜젝션을 실행 후 테스트가 끝날때 롤백해줌
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {//테스트는 한국어도 ㄱㅊ 
+        // given :입력
+        Member member = new Member();
+        member.setName("hello");
+        //when : 로직
+        Long saveId = memberService.join(member);
+        //then: 출력
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
+    }
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
